f69c15ddb3d10c1d5cc1968aa2df88314748c0a2
KZNS <kzns@foxmail.com>
2021-05-28 20:17:56 +0800

    feat: add delProcessInfoThis()

--- scheduling.js
+++ scheduling.js
@@ -21,6 +21,7 @@ function getPageElements() {
 function newProcess() {
     console.log("do newProcess()");
     var p = {
+        processId: "",
         arrivalTime: null,
         serviceTime: null,
         completionTime: completionTimeDefault,
@@ -32,6 +33,7 @@ function newProcess() {
 
 function formatFitProcess(i) {
     processInfoFormat.find("th").text(i + 1);
+    processInfoFormat.attr("id", processes[i].processId);
     processInfoFormat.find(".arrivalTime").attr("value", processes[i].arrivalTime);
     processInfoFormat.find(".serviceTime").attr("value", processes[i].serviceTime);
     processInfoFormat.find(".completionTime").text(processes[i].completionTime);
@@ -41,10 +43,12 @@ function formatFitProcess(i) {
 
 function updateTable() {
     console.log("do updateTable()");
+
     var processInfosTbody = $("#processInfos").find("tbody");
     processInfosTbody.empty();
 
     for (i = 0; i < processes.length; i++) {
+        processes[i].processId = "process" + i;
         formatFitProcess(i);
         processInfosTbody.append(processInfoFormat.prop('outerHTML'));
     }
@@ -54,6 +58,22 @@ function newProcessInfo() {
     console.log("do newProcessInfo()");
 
     processes.push(newProcess());
+    updateTable();
+}
+function delProcessInfoThis(data) {
+    console.log("do delProcessInfoThis()");
+
+    var processId = $(data).parents("tr").attr("id");
+    console.log("del " + processId);
+    for (var i = 0; i < processes.length; i++) {
+        if (processes[i].processId == processId) {
+            processes.splice(i, 1);
+            break;
+        }
+    }
+    if (processes.length == 0) {
+        processes.push(newProcess());
+    }
     updateTable();
 }
 function delProcessInfoLast() {
