cd134f9f9ab08080d8dedcc3ba76f115607c7dcd
KZNS <kzns@foxmail.com>
2021-06-01 22:04:01 +0800

    docs: add comments, change code block position

--- scheduling.js
+++ scheduling.js
@@ -1,24 +1,13 @@
-var NonNegInt = /^(0|[1-9]\d*)$/;
-
-var processes = [];
-var orderedProcesses = [];
-var commited = false;
-var hasWrongProcessInfo = false;
+/* 页面初始化
+ */
+var processInfoFormat;
+var processProgressFormat;
 
 var arrivalTimeDefault;
 var serviceTimeDefault;
 var completionTimeDefault;
 var turnaroundTimeDefault;
 var turnaroundTimeWeightDefault;
-
-var processInfoFormat;
-var processProgressFormat;
-
-var algorithm;
-
-var processProgressMax;
-var SimulationClock;
-var simulating = false;
 
 function getPageElements() {
     console.log("do getPageElements()");
@@ -33,7 +22,26 @@ function getPageElements() {
 
     processProgressFormat = $('.processProgress:first').clone();
 }
+function initPage() {
+    console.log("do initPage()");
 
+    getPageElements();
+    processes.push(newProcess());
+    updateTable();
+}
+
+/* 算法选择
+ */
+var algorithm;
+function changeAlgorithm() {
+    console.log("do changeAlgorithm()");
+    uncommitProcessInfos();
+    updateTable();
+}
+
+/* 进程信息表格
+ */
+var processes = [];
 function newProcess() {
     console.log("do newProcess()");
     var p = {
@@ -49,42 +57,6 @@ function newProcess() {
     };
     return p;
 }
-
-function processInfoFormatFit(i) {
-    processInfoFormat.find("th").text(i + 1);
-    processInfoFormat.attr("id", processes[i].processId);
-    processInfoFormat.find(".arrivalTime").attr("value", processes[i].arrivalTime);
-    processInfoFormat.find(".serviceTime").attr("value", processes[i].serviceTime);
-    processInfoFormat.find(".completionTime").text(processes[i].completionTime);
-    processInfoFormat.find(".turnaroundTime").text(processes[i].turnaroundTime);
-    processInfoFormat.find(".turnaroundTimeWeight").text(processes[i].turnaroundTimeWeight);
-}
-function processProgressFormatFit(i) {
-    var p = processes[i];
-    processProgressFormat.attr('id', p.progressId);
-    processProgressFormat.find('th').text(i + 1);
-    processProgressFormat.find('.progress').css('width', (p.serviceTime / processProgressMax * 80) + '%');
-    processProgressFormat.find('.progress-bar').css('width', (p.remainingTime / p.serviceTime * 100) + '%');
-    processProgressFormat.find('.progress-bar').text(p.remainingTime);
-    processProgressFormat.find('.serviceTime').text(p.serviceTime);
-}
-
-function updateTable() {
-    console.log("do updateTable()");
-
-    var processInfosTbody = $("#processInfos").find("tbody");
-    processInfosTbody.empty();
-
-    for (i = 0; i < processes.length; i++) {
-        processes[i].id = i;
-        processes[i].processId = "process" + i;
-        processInfoFormatFit(i);
-        processInfosTbody.append(processInfoFormat.prop('outerHTML'));
-    }
-    hasWrongProcessInfo = false;
-    processInfosTbody.find("input").trigger("oninput");
-}
-
 function newProcessInfo() {
     console.log("do newProcessInfo()");
 
@@ -129,14 +101,37 @@ function delProcessInfoAll() {
     updateTable();
 }
 
-function initPage() {
-    console.log("do initPage()");
+/* 进程信息表格显示
+ */
+function processInfoFormatFit(i) {
+    processInfoFormat.find("th").text(i + 1);
+    processInfoFormat.attr("id", processes[i].processId);
+    processInfoFormat.find(".arrivalTime").attr("value", processes[i].arrivalTime);
+    processInfoFormat.find(".serviceTime").attr("value", processes[i].serviceTime);
+    processInfoFormat.find(".completionTime").text(processes[i].completionTime);
+    processInfoFormat.find(".turnaroundTime").text(processes[i].turnaroundTime);
+    processInfoFormat.find(".turnaroundTimeWeight").text(processes[i].turnaroundTimeWeight);
+}
+function updateTable() {
+    console.log("do updateTable()");
 
-    getPageElements();
-    processes.push(newProcess());
-    updateTable();
+    var processInfosTbody = $("#processInfos").find("tbody");
+    processInfosTbody.empty();
+
+    for (i = 0; i < processes.length; i++) {
+        processes[i].id = i;
+        processes[i].processId = "process" + i;
+        processInfoFormatFit(i);
+        processInfosTbody.append(processInfoFormat.prop('outerHTML'));
+    }
+    hasWrongProcessInfo = false;
+    processInfosTbody.find("input").trigger("oninput");
 }
 
+/* 输入信息处理
+ */
+var NonNegInt = /^(0|[1-9]\d*)$/;
+var hasWrongProcessInfo = false;
 function checkNonNegInt(data) {
     var value = $(data).val();
     if (NonNegInt.test(value) || (!commited && value == "")) {
@@ -151,7 +146,6 @@ function checkNonNegInt(data) {
         }
     }
 }
-
 function bindingProcess(data) {
     console.log("do bindingProcess()");
     var value = $(data).val();
@@ -174,6 +168,8 @@ function bindingProcess(data) {
     console.log(processes[index]);
 }
 
+/* 批量读入和保存进程信息
+ */
 function initModal(data) {
     console.log("do initModal()");
     var target = $(data).attr("data-bs-target");
@@ -212,214 +208,6 @@ function saveProcessInfos() {
 
     navigator.clipboard.writeText(infos)
 }
-
-function changeAlgorithm() {
-    console.log("do changeAlgorithm()");
-    uncommitProcessInfos();
-    updateTable();
-}
-
-function commitProcessInfos() {
-    commited = true;
-    updateTable();
-    if (hasWrongProcessInfo) {
-        console.log("hasWrongProcessInfo");
-        return;
-    }
-    commited = false;
-    algorithm = $("select").val();
-    console.log(algorithm);
-
-    getOrderedProcesses();
-
-    calculate();
-    updateTable();
-
-    initSimulation();
-    renderSimulation();
-}
-function uncommitProcessInfos() {
-    uncalculate();
-    simulating = false;
-}
-function getOrderedProcesses() {
-    console.log('do getOrderedProcesses()');
-    console.log(processes);
-    orderedProcesses = [];
-    for (var i = 0; i < processes.length; i++) {
-        processes[i].arrivalTime = parseInt(processes[i].arrivalTime);
-        processes[i].serviceTime = parseInt(processes[i].serviceTime);
-        orderedProcesses.push({
-            id: i,
-            process: processes[i],
-            arrivalTime: processes[i].arrivalTime,
-            serviceTime: processes[i].serviceTime
-        });
-    }
-    orderedProcesses.sort(
-        function (a, b) {
-            if (a.arrivalTime == b.arrivalTime) {
-                return a.id - b.id;
-            }
-            else {
-                return a.arrivalTime - b.arrivalTime;
-            }
-        }
-    )
-    console.log(orderedProcesses);
-
-}
-function calculate() {
-    console.log("do calculate()");
-    if (algorithm == 'FCFS') {
-        calculateFCFS(orderedProcesses);
-    }
-    else if (algorithm == 'RR') {
-        calculateRR(orderedProcesses);
-    }
-    else if (algorithm == 'SJF') {
-        calculateSJF(orderedProcesses);
-    }
-    else if (algorithm == 'HRN') {
-        calculateHRN(orderedProcesses);
-    }
-    calculateRest();
-}
-function uncalculate() {
-    console.log("do uncalculate()");
-    for (var i = 0; i < processes.length; i++) {
-        processes[i].completionTime = completionTimeDefault;
-        processes[i].turnaroundTime = turnaroundTimeDefault;
-        processes[i].turnaroundTimeWeight = turnaroundTimeWeightDefault;
-    }
-}
-
-function calculateRest() {
-    for (var i = 0; i < processes.length; i++) {
-        var process = processes[i];
-        process.turnaroundTime = process.completionTime - process.arrivalTime;
-        process.turnaroundTimeWeight = (process.turnaroundTime / process.serviceTime).toFixed(2);
-    }
-}
-function calculateFCFS(ls) {
-    console.log("calculating FCFS");
-    var clock = 0;
-    for (var i = 0; i < ls.length; i++) {
-        clock = Math.max(clock, ls[i].arrivalTime);
-        processes[ls[i].id].completionTime = clock + ls[i].serviceTime;
-        clock += ls[i].serviceTime;
-    }
-}
-function calculateRR(ls) {
-    console.log("calculating RR");
-    var clock = 0;
-    var RRls = [];
-    for (var i = 0; i < ls.length; i++) {
-        if (RRls.length != 0) {
-            var minRemainningTime = RRls[0].remainingTime;
-            for (var j = 1; j < RRls.length; j++) {
-                minRemainningTime = Math.min(minRemainningTime, RRls[j].remainingTime);
-            }
-            var step;
-            step = Math.min(minRemainningTime - 1, Math.floor((ls[i].arrivalTime - clock) / RRls.length))
-            for (var j = 0; j < RRls.length; j++) {
-                RRls[j].remainingTime -= step;
-            }
-            clock += step * RRls.length;
-            while (RRls.length != 0 && clock < ls[i].arrivalTime) {
-                var process = RRls.shift();
-                process.remainingTime--;
-                clock++;
-                if (process.remainingTime > 0) {
-                    RRls.push(process);
-                }
-                else {
-                    processes[process.process.id].completionTime = clock;
-                }
-            }
-        }
-        else {
-            clock = Math.max(clock, ls[i].arrivalTime);
-        }
-
-        RRls.push({ process: ls[i], remainingTime: ls[i].serviceTime });
-    }
-    while (RRls.length != 0) {
-        var minRemainningTime = RRls[0].remainingTime;
-        for (var j = 1; j < RRls.length; j++) {
-            minRemainningTime = Math.min(minRemainningTime, RRls[j].remainingTime);
-        }
-        clock += minRemainningTime * RRls.length;
-        for (var i = 0; i < RRls.length; i++) {
-            RRls[i].remainingTime -= minRemainningTime;
-            if (RRls[i].remainingTime <= 0) {
-                processes[RRls[i].process.id].completionTime = clock - (RRls.length - i - 1);
-                RRls.splice(i, 1);
-                i--;
-            }
-        }
-    }
-}
-function calculateSJF(ls) {
-    console.log("calculating SJF");
-    var clock = 0;
-    var SJFls = [];
-    var i = 0;
-    while (i < ls.length || SJFls.length > 0) {
-        if (SJFls.length == 0) {
-            clock = Math.max(clock, ls[i].arrivalTime);
-        }
-        while (i < ls.length && clock >= ls[i].arrivalTime) {
-            SJFls.push(ls[i]);
-            i++;
-        }
-        var minone = 0;
-        for (var j = 1; j < SJFls.length; j++) {
-            if (SJFls[j].serviceTime < SJFls[minone].serviceTime) {
-                minone = j;
-            }
-        }
-        clock += SJFls[minone].serviceTime;
-        processes[SJFls[minone].id].completionTime = clock;
-        SJFls.splice(minone, 1);
-    }
-}
-function HRNWeight(clock, arrivalTime, serviceTime) {
-    return (clock - arrivalTime + serviceTime) / serviceTime;
-}
-function HRNWeight(clock, p) {
-    return (clock - p.arrivalTime + p.serviceTime) / p.serviceTime;
-}
-function calculateHRN(ls) {
-    console.log("calculating HRN");
-    var clock = 0;
-    var HRNls = [];
-    var i = 0;
-    while (i < ls.length || HRNls.length > 0) {
-        if (HRNls.length == 0) {
-            clock = Math.max(clock, ls[i].arrivalTime);
-        }
-        while (i < ls.length && clock >= ls[i].arrivalTime) {
-            HRNls.push({
-                process: ls[i],
-                weight: 0
-            });
-            i++;
-        }
-        var minone = 0;
-        HRNls[0].weight = HRNWeight(clock, HRNls[0].process.arrivalTime, HRNls[0].process.serviceTime);
-        for (var j = 1; j < HRNls.length; j++) {
-            HRNls[j].weight = HRNWeight(clock, HRNls[j].process.arrivalTime, HRNls[j].process.serviceTime);
-            if (HRNls[j].weight > HRNls[minone].weight) {
-                minone = j;
-            }
-        }
-        clock += HRNls[minone].process.serviceTime;
-        processes[HRNls[minone].process.id].completionTime = clock;
-        HRNls.splice(minone, 1);
-    }
-}
-
 // ref: http://stackoverflow.com/a/1293163/2343
 // This will parse a delimited string into an array of
 // arrays. The default delimiter is the comma, but this
@@ -504,14 +292,238 @@ function CSVToArray(strData, strDelimiter) {
     return (arrData);
 }
 
+/* 数据提交
+ */
+var commited = false;
+function commitProcessInfos() {
+    commited = true;
+    updateTable();
+    if (hasWrongProcessInfo) {
+        console.log("hasWrongProcessInfo");
+        return;
+    }
+    commited = false;
+    algorithm = $("select").val();
+    console.log(algorithm);
 
+    getOrderedProcesses();
+
+    calculate();
+    updateTable();
+
+    initSimulation();
+    renderSimulation();
+}
+function uncommitProcessInfos() {
+    uncalculate();
+    simulating = false;
+}
+
+/* 计算
+ */
+var orderedProcesses = [];
+function getOrderedProcesses() {
+    console.log('do getOrderedProcesses()');
+    console.log(processes);
+    orderedProcesses = [];
+    for (var i = 0; i < processes.length; i++) {
+        processes[i].arrivalTime = parseInt(processes[i].arrivalTime);
+        processes[i].serviceTime = parseInt(processes[i].serviceTime);
+        orderedProcesses.push({
+            id: i,
+            process: processes[i],
+            arrivalTime: processes[i].arrivalTime,
+            serviceTime: processes[i].serviceTime
+        });
+    }
+    orderedProcesses.sort(
+        function (a, b) {
+            if (a.arrivalTime == b.arrivalTime) {
+                return a.id - b.id;
+            }
+            else {
+                return a.arrivalTime - b.arrivalTime;
+            }
+        }
+    )
+    console.log(orderedProcesses);
+
+}
+function calculate() {
+    console.log("do calculate()");
+    if (algorithm == 'FCFS') {
+        calculateFCFS(orderedProcesses);
+    }
+    else if (algorithm == 'RR') {
+        calculateRR(orderedProcesses);
+    }
+    else if (algorithm == 'SJF') {
+        calculateSJF(orderedProcesses);
+    }
+    else if (algorithm == 'HRN') {
+        calculateHRN(orderedProcesses);
+    }
+    calculateRest();
+}
+function uncalculate() {
+    console.log("do uncalculate()");
+    for (var i = 0; i < processes.length; i++) {
+        processes[i].completionTime = completionTimeDefault;
+        processes[i].turnaroundTime = turnaroundTimeDefault;
+        processes[i].turnaroundTimeWeight = turnaroundTimeWeightDefault;
+    }
+}
+
+function HRNWeight(clock, arrivalTime, serviceTime) {
+    return (clock - arrivalTime + serviceTime) / serviceTime;
+}
+function HRNWeight(clock, p) {
+    return (clock - p.arrivalTime + p.serviceTime) / p.serviceTime;
+}
+function calculateFCFS(ls) {
+    console.log("calculating FCFS");
+    var clock = 0;
+    for (var i = 0; i < ls.length; i++) {
+        clock = Math.max(clock, ls[i].arrivalTime);
+        processes[ls[i].id].completionTime = clock + ls[i].serviceTime;
+        clock += ls[i].serviceTime;
+    }
+}
+function calculateRR(ls) {
+    console.log("calculating RR");
+    var clock = 0;
+    var RRls = [];
+    for (var i = 0; i < ls.length; i++) {
+        if (RRls.length != 0) {
+            var minRemainningTime = RRls[0].remainingTime;
+            for (var j = 1; j < RRls.length; j++) {
+                minRemainningTime = Math.min(minRemainningTime, RRls[j].remainingTime);
+            }
+            var step;
+            step = Math.min(minRemainningTime - 1, Math.floor((ls[i].arrivalTime - clock) / RRls.length))
+            for (var j = 0; j < RRls.length; j++) {
+                RRls[j].remainingTime -= step;
+            }
+            clock += step * RRls.length;
+            while (RRls.length != 0 && clock < ls[i].arrivalTime) {
+                var process = RRls.shift();
+                process.remainingTime--;
+                clock++;
+                if (process.remainingTime > 0) {
+                    RRls.push(process);
+                }
+                else {
+                    processes[process.process.id].completionTime = clock;
+                }
+            }
+        }
+        else {
+            clock = Math.max(clock, ls[i].arrivalTime);
+        }
+
+        RRls.push({ process: ls[i], remainingTime: ls[i].serviceTime });
+    }
+    while (RRls.length != 0) {
+        var minRemainningTime = RRls[0].remainingTime;
+        for (var j = 1; j < RRls.length; j++) {
+            minRemainningTime = Math.min(minRemainningTime, RRls[j].remainingTime);
+        }
+        clock += minRemainningTime * RRls.length;
+        for (var i = 0; i < RRls.length; i++) {
+            RRls[i].remainingTime -= minRemainningTime;
+            if (RRls[i].remainingTime <= 0) {
+                processes[RRls[i].process.id].completionTime = clock - (RRls.length - i - 1);
+                RRls.splice(i, 1);
+                i--;
+            }
+        }
+    }
+}
+function calculateSJF(ls) {
+    console.log("calculating SJF");
+    var clock = 0;
+    var SJFls = [];
+    var i = 0;
+    while (i < ls.length || SJFls.length > 0) {
+        if (SJFls.length == 0) {
+            clock = Math.max(clock, ls[i].arrivalTime);
+        }
+        while (i < ls.length && clock >= ls[i].arrivalTime) {
+            SJFls.push(ls[i]);
+            i++;
+        }
+        var minone = 0;
+        for (var j = 1; j < SJFls.length; j++) {
+            if (SJFls[j].serviceTime < SJFls[minone].serviceTime) {
+                minone = j;
+            }
+        }
+        clock += SJFls[minone].serviceTime;
+        processes[SJFls[minone].id].completionTime = clock;
+        SJFls.splice(minone, 1);
+    }
+}
+function calculateHRN(ls) {
+    console.log("calculating HRN");
+    var clock = 0;
+    var HRNls = [];
+    var i = 0;
+    while (i < ls.length || HRNls.length > 0) {
+        if (HRNls.length == 0) {
+            clock = Math.max(clock, ls[i].arrivalTime);
+        }
+        while (i < ls.length && clock >= ls[i].arrivalTime) {
+            HRNls.push({
+                process: ls[i],
+                weight: 0
+            });
+            i++;
+        }
+        var minone = 0;
+        HRNls[0].weight = HRNWeight(clock, HRNls[0].process.arrivalTime, HRNls[0].process.serviceTime);
+        for (var j = 1; j < HRNls.length; j++) {
+            HRNls[j].weight = HRNWeight(clock, HRNls[j].process.arrivalTime, HRNls[j].process.serviceTime);
+            if (HRNls[j].weight > HRNls[minone].weight) {
+                minone = j;
+            }
+        }
+        clock += HRNls[minone].process.serviceTime;
+        processes[HRNls[minone].process.id].completionTime = clock;
+        HRNls.splice(minone, 1);
+    }
+}
+function calculateRest() {
+    for (var i = 0; i < processes.length; i++) {
+        var process = processes[i];
+        process.turnaroundTime = process.completionTime - process.arrivalTime;
+        process.turnaroundTimeWeight = (process.turnaroundTime / process.serviceTime).toFixed(2);
+    }
+}
+
+
+/* 模拟
+ */
 var simulationLs = [];
-var processing = -1;
+var SimulationClock;
+var simulating = false;
 
+var processing = -1;
+var processProgressMax;
+
+/* 模拟显示
+ */
 function newSimulationItem(p) {
     return { process: p, weight: 0 };
 }
-
+function processProgressFormatFit(i) {
+    var p = processes[i];
+    processProgressFormat.attr('id', p.progressId);
+    processProgressFormat.find('th').text(i + 1);
+    processProgressFormat.find('.progress').css('width', (p.serviceTime / processProgressMax * 80) + '%');
+    processProgressFormat.find('.progress-bar').css('width', (p.remainingTime / p.serviceTime * 100) + '%');
+    processProgressFormat.find('.progress-bar').text(p.remainingTime);
+    processProgressFormat.find('.serviceTime').text(p.serviceTime);
+}
 function initSimulation() {
     console.log('do initSimulation()')
     processProgressMax = 0;
@@ -547,6 +559,8 @@ function updateSimulation() {
     $('#SimulationClock').val(SimulationClock);
 }
 
+/* 逐步模拟
+ */
 function nextStep() {
     console.log('do nextStep()');
     if (!simulating) {
@@ -555,7 +569,6 @@ function nextStep() {
     nextClock();
     updateSimulation();
 }
-
 function nextClock() {
     console.log('do nextClock()');
     if (algorithm == 'FCFS') {
