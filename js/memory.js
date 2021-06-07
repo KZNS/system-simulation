5ff0bc45ddd67db9b200281d1fc28345fb30c1c0
KZNS <kzns@foxmail.com>
2021-06-07 23:44:31 +0800

    feat(memory): add newEventInfo() s

--- js/memory.js
+++ js/memory.js
@@ -56,6 +56,55 @@ function newEvent() {
     }
     return e;
 }
+/**
+ * 添加一行表格在最后
+ */
+function newEventInfo() {
+    console.log("do newEventInfo()");
+
+    events.push(newEvent());
+    var evn = events[events.length - 1];
+    evn.setID(events.length);
+    eventInfoFormat.find('th').text(evn.id);
+    eventInfoFormat.attr('id', evn.eventID);
+    $('#eventInfos tbody').append(eventInfoFormat.prop('outerHTML'));
+}
+/**
+ * 删除表格当前行
+ * @param {DOM} data 当前DOM元素
+ */
+function delEventInfoThis(data) {
+    console.log("do delEventInfoThis()");
+
+    var eventID = $(data).parents("tr").attr("id");
+    console.log("del " + eventID);
+    $('#eventInfos #' + eventID).remove();
+    var id = parseInt(eventID.replace(/[^0-9]/ig, ""));
+    events.splice(id - 1, 1);
+    updateTable();
+}
+/**
+ * 删除表格最后一行
+ */
+function delEventInfoLast() {
+    console.log("do delEventInfoLast()");
+
+    events.pop();
+    $('#eventInfos tbody tr:last').remove();
+    if (events.length == 0) {
+        newEventInfo();
+    }
+}
+/**
+ * 删除全部表格
+ */
+function delEventInfoAll() {
+    console.log("do delEventInfoAll()");
+
+    events = [];
+    $('#eventInfos tbody').empty();
+    newEventInfo();
+}
 
 // --------------------------------
 // 事件信息表格显示
@@ -71,9 +120,10 @@ function updateTable() {
     for (var i = 0; i < events.length; i++) {
         var evn = events[i];
         var eventInfoTr = eventInfosTbody.find('#' + evn.eventID);
-        if (evn.id != i + 1) {
-            evn.setID(i);
-            eventInfoTr.arrt('id', evn.eventID);
+        if (evn.id !== i + 1) {
+            evn.setID(i + 1);
+            eventInfoTr.attr('id', evn.eventID);
+            eventInfoTr.find('th').text(evn.id);
         }
     }
 }
